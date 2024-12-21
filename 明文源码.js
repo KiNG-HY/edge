2fd9008449f5f002b60b8fc22083d827438bdbe9
KiNG-HY <129816564+KiNG-HY@users.noreply.github.com>
2024-12-21 22:44:11 +0800

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -1,7 +1,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let userID = '';
+let userID = 'KiNG-QQ:3148213528';
 let proxyIP = '';
 let sub = '';
 let subConverter = 'SUBAPI.fxxk.dedyn.io';
@@ -1440,8 +1440,6 @@ async function 生成配置信息(userID, hostName, sub, UA, RproxyIP, _url, env
 			---------------------------------------------------------------<br>
 			${clash}<br>
 			---------------------------------------------------------------<br>
-			################################################################<br>
-			${cmad}
 			`;
 		return 节点配置页;
 	} else {
